cdf90300045e6b2ff65991145fe27ef261c99685
cedric duvillier <ced_duv@hotmail.com>
2021-11-15 17:25:42 +0100

    mvp htm routing

---
+++ db/migrate/20211115144610_create_pokemons.rb
@@ -0,0 +1,10 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
