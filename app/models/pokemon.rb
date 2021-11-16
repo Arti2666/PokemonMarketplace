143a1f45d08f9700d81714b6632b9a70a3ec06da
cedric duvillier <ced_duv@hotmail.com>
2021-11-16 12:35:47 +0100

    fin models controller

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,4 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 end
---
+++ db/migrate/20211116095716_changetransaactionname.rb
@@ -0,0 +1,5 @@
+class Changetransaactionname < ActiveRecord::Migration[6.0]
+  def change
+    rename_table :transactions, :bookings
+  end
+end
