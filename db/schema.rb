681b82d1c51b9366f73ba23c04925f89a870a983
Arthur Perissich <arthurp@alumni.ie.edu>
2021-11-17 13:21:02 +0100

    Scraping and DescriptionToPokeMoodel migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_133328) do
+ActiveRecord::Schema.define(version: 2021_11_17_120430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_133328) do
     t.string "type2", default: ""
     t.integer "price"
     t.boolean "reserved", default: false
+    t.string "description"
     t.index ["user_id"], name: "index_pokemons_on_user_id"
   end
 
